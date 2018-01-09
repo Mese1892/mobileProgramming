9fa6994e587f6d44c5ba9c86d4aec9f3e103402f
Catalin <andreimesesan@yahoo.com>
2018-01-09 03:40:43 +0200

    Lab7.Final. Here's hoping for 5.

--- UclAppAndroid/app/src/main/java/com/home/cata/uclappandroid/LoginActivity.java
+++ UclAppAndroid/app/src/main/java/com/home/cata/uclappandroid/LoginActivity.java
@@ -1,10 +1,20 @@
 package com.home.cata.uclappandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by Catalin on 1/9/2018.
@@ -12,13 +22,68 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private static  final String LG="FIREBASE AUTH";
+    private static final String LG = "FIREBASE AUTH";
     private FirebaseAuth firebaseAuth;
     private FirebaseAuth.AuthStateListener listener;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
 
+        final EditText emailAdress = (EditText) findViewById(R.id.emailEditText);
+        final EditText password = (EditText) findViewById(R.id.passwordEdit);
+        final Button signInbtn = (Button) findViewById(R.id.signInButton);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        listener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser usr = firebaseAuth.getCurrentUser();
+                if (usr != null){
+                    Intent i= new Intent(LoginActivity.this, MainActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(i);
+                }
+            }
+        };
+
+        signInbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email= emailAdress.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                if(email != null && pass!=null){
+                    signIn(email, pass);
+                }
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(listener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(listener);
+    }
+
+   public  void signIn(String email, String pass) {
+
+        firebaseAuth.signInWithEmailAndPassword(email,pass)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this, "Authfailed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
     }
 }
--- UclAppAndroid/app/src/main/java/com/home/cata/uclappandroid/MainActivity.java
+++ UclAppAndroid/app/src/main/java/com/home/cata/uclappandroid/MainActivity.java
@@ -5,12 +5,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Logger;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class MainActivity extends Activity {
+
+    DatabaseReference userDB;
+    FirebaseDatabase database;
+    String role;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +30,24 @@ public class MainActivity extends Activity {
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
         FirebaseDatabase.getInstance().setLogLevel(Logger.Level.DEBUG);
 
+        //userDB = database.getReference().child("users");
+
+//        userDB.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                role = dataSnapshot.getValue(String.class);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+
         Button viewTeams = (Button) findViewById(R.id.ViewTeamsButton);
         Button sendEmail = (Button) findViewById(R.id.EmailButton);
-        Button book=(Button) findViewById(R.id.book_button);
+        Button book = (Button) findViewById(R.id.book_button);
+        Button logout = (Button) findViewById(R.id.SignOutButton);
 
         viewTeams.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,8 +68,25 @@ public class MainActivity extends Activity {
         book.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(MainActivity.this, BookTeam.class);
-                startActivity(i);
+               // if(!role.equals("admin")){
+                    Intent i = new Intent(MainActivity.this, BookTeam.class);
+                    startActivity(i);
+                //}else{
+                  //  Toast.makeText(MainActivity.this, "Not available as admin", Toast.LENGTH_SHORT).show();
+                //}
+
+            }
+        });
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                    Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(i);
+                }
             }
         });
     }
